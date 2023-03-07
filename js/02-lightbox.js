6791397a94a21178c4f436c1c27fbccc39a25b42
Oksana Teliupa <oxyrooby@gmail.com>
2023-03-07 18:03:13 +0200

    2task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const makeGalleryCard = ({preview, original, description}) => `
+<a class="gallery-link" href="${original}">
+<img class="gallery-image" loading="lazy" src="${preview}" data-sourse="${original}" alt="${description}" width=350>
+`
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map((data) => makeGalleryCard(data)).join('');
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox(".gallery a", {
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+   
+
